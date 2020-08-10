4568c31c2c0122a2c9fab1ab6e747b8098d53ee4
frankluongo <frank@frankluongo.com>
2020-08-10 08:47:28 -0400

    Cleaning up some code

--- src/resolvers/index.js
+++ src/resolvers/index.js
@@ -1,29 +1,9 @@
+const movieResolvers = require("./resMovies");
+
 const resolvers = {
   Query: {
-    movies: () => {
-      return movies;
-    },
-    movie: getSingleMovie,
+    ...movieResolvers,
   },
 };
-
-const movies = [
-  {
-    id: "1",
-    title: "Godzilla",
-    releaseDate: "11-03-1954",
-    rating: 93,
-  },
-  {
-    id: "2",
-    title: "Godzilla vs Mechagodzilla",
-    releaseDate: "03-21-1974",
-    rating: 71,
-  },
-];
-
-function getSingleMovie(_, { id }) {
-  return movies.find((mov) => mov.id === id);
-}
 
 module.exports = resolvers;
---
+++ src/resolvers/resMovies.js
@@ -0,0 +1,29 @@
+const movieResolvers = {
+  movies,
+  movie,
+};
+
+function movies() {
+  return moviesData;
+}
+
+function movie(_, { id }) {
+  return moviesData.find((mov) => mov.id === id);
+}
+
+const moviesData = [
+  {
+    id: "1",
+    title: "Godzilla",
+    releaseDate: "11-03-1954",
+    rating: 93,
+  },
+  {
+    id: "2",
+    title: "Godzilla vs Mechagodzilla",
+    releaseDate: "03-21-1974",
+    rating: 71,
+  },
+];
+
+module.exports = movieResolvers;
